96d32afd123ae273aed6080c85c1968daccaa418
MiriamPeskowitz <miriam5000@gmail.com>
2020-07-15 22:01:17 -0400

    Adds new design features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_194232) do
+ActiveRecord::Schema.define(version: 2020_07_16_010013) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
